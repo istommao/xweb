8faedb48ff7c4cc886694af561da54a45b9bf9fb
wuyou <wuyou@goumee.com>
2021-06-21 11:42:46 +0800

    Add vue3 setup project

---
+++ xweb/vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: '0.0.0.0'
+  },
+  define: {
+    'process.env': process.env
+  },
+  base: './', // 设置打包路径
+  resolve: {
+    // 导入文件夹别名
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      views: path.resolve(__dirname, './src/views'),
+      components: path.resolve(__dirname, './src/components'),
+      utils: path.resolve(__dirname, './src/utils'),
+      less: path.resolve(__dirname, "./src/less"),
+      assets: path.resolve(__dirname, "./src/assets"),
+      com: path.resolve(__dirname, "./src/components"),
+      store: path.resolve(__dirname, "./src/store"),
+      mixins: path.resolve(__dirname, "./src/mixins")
+    },
+  }
+})
